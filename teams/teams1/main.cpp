9a6d74055296b55a30af61bff5854be229fd854f
kou <41243133@nfu.edu.tw>
2024-10-08 22:44:56 +0800

    還原被刪除的組長跟組員1

--- teams/teams1/main.cpp
+++ teams/teams1/main.cpp
@@ -18,6 +18,25 @@ int main(int argc, char *argv[]) {
     LeaderLayout->addWidget(LeaderLabel);
     LeaderTab->setLayout(LeaderLayout);
 
+    
+    QWidget *member1Tab = new QWidget;
+    QLabel *member1Label = new QLabel("這是組員1頁面");
+    QPushButton *colorButton = new QPushButton("color select");
+    QVBoxLayout *member1Layout = new QVBoxLayout;
+    member1Layout->addWidget(member1Label);
+    member1Layout->addWidget(colorButton);
+    member1Tab->setLayout(member1Layout);
+
+
+    QObject::connect(colorButton, &QPushButton::clicked, [=]() {
+        QColor color = QColorDialog::getColor(Qt::white, nullptr, "選擇文字顏色");
+        if (color.isValid()) {
+            QString colorName = color.name();
+            leaderLabel->setStyleSheet(QString("color: %1;").arg(colorName));
+        }
+    });
+
+
    QWidget *member2Tab = new QWidget;
     QLabel *member2Label = new QLabel("這是組員2頁面");
     QPushButton *styleButton = new QPushButton("Front select");
